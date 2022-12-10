eef924caa8a92a9abc639b89071e4390db00c6f2
angelinesimiyu <angie.lukelas@gmail.com>
2022-12-10 09:47:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (i = 0; i < (array.length - 1 ); i++) {
+    for (j = (i + 1); j < array.length; j++) {
+
+        if (array[i] + array[j] == target) {
+            return true;
+        }
+    }
 }
 
-/* 
+return false;
+
+
+}
+
+/*
   Write the Big O time complexity of your function here
 */
-
-/* 
+'Time Complexity: O(N**2)'
+/*
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
 */
+'The sum of a pair of numbers in an array should equal the target integer'
+  'if there is a pair in the arry that meets the condition return true'
+  'else return false'
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
